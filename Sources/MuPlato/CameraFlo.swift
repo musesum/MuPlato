599023f4dd2c9da2aa042a6e80cf76c5a95881a4
musesum <musesum@gmail.com>
2023-06-28 15:30:11 +0800

    sync MuLeaf, camera facing

--- Sources/MuPlato/CameraFlo.swift
+++ Sources/MuPlato/CameraFlo.swift
@@ -8,14 +8,14 @@ open class CameraFlo {
     static public let shared = CameraFlo()
 
     private var stream˚: Flo? ; var stream = false
-    private var facing˚: Flo? ; var facing = false
+    private var front˚ : Flo? ; var front  = false
     private var mask˚  : Flo? ; var mask   = false
 
     init() {
 
         let camera = Flo.root˚.bind("model.camera")
         stream˚ = camera.bind("stream") { f,_ in self.stream = f.bool }
-        facing˚ = camera.bind("facing") { f,_ in self.facing = f.bool }
+        front˚  = camera.bind("front" ) { f,_ in self.front  = f.bool }
         mask˚   = camera.bind("mask"  ) { f,_ in self.mask   = f.bool }
     }
 }
