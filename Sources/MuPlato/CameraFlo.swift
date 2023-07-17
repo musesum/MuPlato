ae3538532e4732bfe901be41e29fe093fa411dcc
musesum <musesum@gmail.com>
2023-07-17 12:56:22 +0800

    refactor

--- Sources/MuPlato/CameraFlo.swift
+++ Sources/MuPlato/CameraFlo.swift
@@ -9,13 +9,13 @@ open class CameraFlo {
 
     private var stream˚: Flo? ; var stream = false
     private var front˚ : Flo? ; var front  = false
-    private var mask˚  : Flo? ; var mask   = false
+    //private var mask˚  : Flo? ; var mask   = false
 
     init() {
 
         let camera = Flo.root˚.bind("model.camera")
         stream˚ = camera.bind("stream") { f,_ in self.stream = f.bool }
         front˚  = camera.bind("front" ) { f,_ in self.front  = f.bool }
-        mask˚   = camera.bind("mask"  ) { f,_ in self.mask   = f.bool }
+        //mask˚   = camera.bind("mask"  ) { f,_ in self.mask   = f.bool }
     }
 }
