ed0c4529c47f3b68422c952293e8c582d607651b
musesum <musesum@gmail.com>
2023-07-29 17:49:03 +0800

    CubicPoly interim

--- Sources/MuPlato/MuMetDisplayLink.swift
+++ Sources/MuPlato/MuMetDisplayLink.swift
@@ -8,30 +8,24 @@ protocol MetDisplayLinkFire {
     func didFire()
 }
 class MetDisplayLink {
-
+    
     var link: CADisplayLink?
     var linkFire: MetDisplayLinkFire
     var fps: NSInteger = 60
-
+    
     init(_ linkFire: MetDisplayLinkFire, fps: Int = 60) {
         self.linkFire = linkFire
         self.fps = fps
-        addDisplayLink()
-    }
-
-    /// not used, right now
-    func addDisplayLink() {
-        // link = UIScreen.main.displayLink(withTarget: self, selector: #selector(displayLinkDidFire))
         link = CADisplayLink(target: self, selector: #selector(displayLinkDidFire))
         link?.preferredFramesPerSecond = fps
         link?.add(to: RunLoop.current, forMode: .default)
     }
-
+    
     /// not used, right now
     @objc func displayLinkDidFire() -> Bool {
         linkFire.didFire()
         return true
     }
-
-
+    
+    
 }
